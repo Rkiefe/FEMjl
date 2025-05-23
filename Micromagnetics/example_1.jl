9488723aa9df0a53d645991cee7e875c0169c1ec
Rkiefe <kiefe.trabalho@gmail.com>
2025-05-23 14:46:25 +0100

    example_1 now is an example of a time evolution of <m>

--- Micromagnetics/example_1.jl
+++ Micromagnetics/example_1.jl
@@ -1,47 +1,50 @@
 #=
-    Minimizes the energy by steepest descent and by the 
-    Landau-Lifshitz equation.
+    Solves the time dependent Landau-Lifshitz equation
+    Considering a permalloy like in the Fig. 2 of this article
+        https://doi.org/10.1109/TMAG.2008.2001666
 =#
 
 
 # For plots
 using GLMakie
 
-
-include("SteepestDescent.jl") # For the steepest descent algorithm
+include("../gmsh_wrapper.jl")
+include("LandauLifshitz.jl")
 
 function main()
-    meshSize::Float64 = 2_500
-    localSize::Float64 = 50
+    meshSize::Float64 = 2_000
+    localSize::Float64 = 25
 
     # Constants
     mu0::Float64 = pi*4e-7          # vacuum magnetic permeability
     giro::Float64 = 2.210173e5 /mu0 # Gyromagnetic ratio (rad T-1 s-1)
-    dt::Float64 = 0.028/giro        # Time step (s)
-    damp::Float64 = 1.0             # Damping parameter (dimensionless [0,1])
-    precession::Bool = false         # Include precession or not
+    dt::Float64 = 0.6e-13        # Time step (s)
+    totalTime::Float64 = 0.4        # Total time of spin dynamics simulation (ns)
+    damp::Float64 = 0.1             # Damping parameter (dimensionless [0,1])
+    precession::Float64 = 1.0       # Include precession or not
 
-    # Dimension of the magnetic material (rectangle)
-    L::Vector{Float64} = [512,128,30]
+    # Dimension of the magnetic material 
+    L::Vector{Float64} = [100,100,5]   # (rectangle)
     scl::Float64 = 1e-9                 # scale of the geometry | (m -> nm)
     
     # Conditions
-    Ms::Float64   = 800e3               # Magnetic saturation (A/m)
-    Aexc::Float64 = 13e-12              # Exchange   (J/m)
-    Aan::Float64  = 0.0                 # Anisotropy (J/m3)
+    Ms::Float64   = 860e3              # Magnetic saturation (A/m)
+    Aexc::Float64 = 13e-12                 # Exchange   (J/m)
+    Aan::Float64  = 0.0               # Anisotropy (J/m3)
     uan::Vector{Float64}  = [1,0,0]     # easy axis direction
-    Hap::Vector{Float64}  = [0,0,0]     # A/m
+    Hap::Vector{Float64}  = [0,50e3,0] # A/m
 
     # Convergence criteria | Only used when totalTime != Inf
-    maxTorque::Float64 = 1e-9          # Maximum difference between current and previous <M>
-    maxAtt::Int32 = 10_000             # Maximum number of iterations in the solver
+    maxTorque::Float64 = 1e-6           # Maximum difference between current and previous <M>
+    maxAtt::Int32 = 15_000              # Maximum number of iterations in the solver
     
     # -- Create a geometry --
         gmsh.initialize()
 
         # >> Model
         # Create an empty container
-        container = addSphere([0,0,0],5*maximum(L))
+        container = addSphere([0,0,0],20*maximum(L))
+
         cells = [] # List of cells inside the container
 
         # Get how many surfaces compose the bounding shell
@@ -68,89 +71,122 @@ function main()
         mesh.shell_id = mesh.shell_id[bounding_shell_n_surfaces] # All other, are interior surfaces
 
         # Finalize Gmsh and show mesh properties
+        # gmsh.fltk.run()
         gmsh.finalize()
-        println("Number of elements ",size(mesh.t,2))
-        println("Number of Inside elements ",length(mesh.InsideElements))
-        println("Number of nodes ",size(mesh.p,2))
-        println("Number of Inside nodes ",length(mesh.InsideNodes))
-        println("Number of surface elements ",size(mesh.surfaceT,2))
-        # return
+       
     # -----------------------
+
+    println("Number of elements ",size(mesh.t,2))
+    println("Number of Inside elements ",length(mesh.InsideElements))
+    println("Number of nodes ",size(mesh.p,2))
+    println("Number of Inside nodes ",length(mesh.InsideNodes))
+    println("Number of surface elements ",size(mesh.surfaceT,2))
+    # println("Bounding shell: ",mesh.shell_id)
+    # return
+
+    # viewMesh(mesh)
+    
+    # Volume of elements of each mesh node | Needed for the demagnetizing field
+    Vn::Vector{Float64} = zeros(mesh.nv)
+
+    # Integral of basis function over the domain | Needed for the exchange field
+    nodeVolume::Vector{Float64} = zeros(mesh.nv)
+    
+    for ik in 1:mesh.nInside
+        k = mesh.InsideElements[ik]
+        Vn[mesh.t[:,k]]         .+= mesh.VE[k]
+        nodeVolume[mesh.t[:,k]] .+= mesh.VE[k]/4
+    end
+    Vn = Vn[mesh.InsideNodes]
+    nodeVolume = nodeVolume[mesh.InsideNodes]
 
     # Magnetization field
     m::Matrix{Float64} = zeros(3,mesh.nv)
     m[1,mesh.InsideNodes] .= 1
-    
-    # Random initial magnetization
     # begin
-    #     theta::Vector{Float64} = 2*pi*rand(mesh.nInsideNodes)
-    #     phi::Vector{Float64} = pi*rand(mesh.nInsideNodes)
-    #     for i = 1:mesh.nInsideNodes
+    #     theta::Vector{Float64} = 2*pi.*rand(mesh.nInsideNodes)
+    #     phi::Vector{Float64} = pi.*rand(mesh.nInsideNodes)
+    
+    #     for i in 1:mesh.nInsideNodes
     #         nd = mesh.InsideNodes[i]
-    #         m[:,i] = [sin(phi[i])*cos(theta[i]),sin(phi[i])*sin(theta[i]),cos(phi[i])]
+    #         m[:,nd] =   [
+    #                         sin(theta[i])*cos(phi[i])
+    #                         sin(theta[i])*sin(phi[i])
+    #                         cos(theta[i])
+    #                     ]
+
+    #         m[:,nd] = m[:,nd]./norm(m[:,nd])
     #     end
-    # end
-    
-    # Minimize energy by L. L. equation with damp = 1
-    m, Heff, _, 
-    M_avg, E_time, torque_time = LandauLifshitz(
-                    mesh, 
-                    scl, 
-                    m, 
-                    Ms, 
-                    Aexc, 
-                    Aan, 
-                    uan, 
-                    Hap, 
-                    dt, 
-                    giro, 
-                    damp, 
-                    precession, 
-                    maxTorque, 
-                    maxAtt
-                    )
-    
-    # Steepest descent energy minimization
-    m, Heff, 
-    M_avg, E_time, torque_time = steepestDescent(
-                    mesh,
-                    scl,
-                    m,
-                    Ms,
-                    Aexc,
-                    Aan,
-                    uan,
-                    Hap,
-                    giro,
-                    maxTorque,
-                    maxAtt)
-    
-    # Energy
+    # end # Random initial magnetization
+
+    # Dirichlet boundary condition
+    fixed::Vector{Int32} = findNodes(mesh,"face",mesh.shell_id)
+    free::Vector{Int32} = setdiff(1:mesh.nv,fixed)
+
+    # Stiffness matrix | Demagnetizing field
+    AD = stiffnessMatrix(mesh)
+
+    # Stiffness matrix | Exchange field
+    A = spzeros(mesh.nv,mesh.nv)
+
+    begin
+        Ak::Matrix{Float64} = zeros(4*4,mesh.nt)
+        b::Vector{Float64} = zeros(4)
+        c::Vector{Float64} = zeros(4)
+        d::Vector{Float64} = zeros(4)
+        aux::Matrix{Float64} = zeros(4,4)
+        
+        for ik in 1:mesh.nInside
+            k = mesh.InsideElements[ik]
+            for i in 1:4
+                _,b[i],c[i],d[i] = abcd(mesh.p,mesh.t[:,k],mesh.t[i,k])
+            end
+            aux = mesh.VE[k]*(b*b' + c*c' + d*d')
+            Ak[:,k] = aux[:] # vec(aux)
+        end
+
+        # Update sparse global matrix
+        n = 0
+        for i in 1:4
+            for j in 1:4
+                n += 1
+                A += sparse(Int.(mesh.t[i,:]),Int.(mesh.t[j,:]),Ak[n,:],mesh.nv,mesh.nv)
+            end
+        end
+        
+        # Remove all exterior nodes
+        A = A[mesh.InsideNodes,mesh.InsideNodes]
+
+    end # End of Stiffness Matrix of Exchange field
+
+    # Landau Lifshitz
+    m, Heff::Matrix{Float64},
+    M_avg::Matrix{Float64},
+    E_time::Vector{Float64}, 
+    torque_time::Vector{Float64} = LandauLifshitz(  
+                                                    mesh, m, Ms,
+                                                    Hap, Aexc, Aan,
+                                                    uan, scl, damp, giro,
+                                                    A, AD, Vn, nodeVolume,
+                                                    free, fixed,
+                                                    dt, precession, maxTorque,
+                                                    maxAtt, totalTime
+                                                 )
+
+    time::Vector{Float64} = 1e9*dt .* (1:size(M_avg,2))
+
     fig = Figure()
-    ax = Axis(  fig[1,1], 
-                xlabel = "Time (ns)", 
-                ylabel = "Energy",
-                title = "")
-    scatter!(ax,1:length(E_time),E_time) # E_time
-
-    # Log of torque
-    ax = Axis(  fig[1,2], 
-                xlabel = "Time (ns)", 
-                ylabel = "Log(Torque)",
-                title = "")
-    scatter!(ax,1:length(torque_time),log10.(torque_time)) # E_time
-
-    # Magnetization
-    ax = Axis(  fig[1,3], 
+    ax = Axis(  fig[1,1],
                 xlabel = "Time (ns)", 
                 ylabel = "<M> (kA/m)",
-                title = "Average Magnetization")
+                title = "Micromagnetic simulation")
 
-    scatter!(ax,1:size(M_avg,2),Ms/1000 .*M_avg[1,:], label = "M_x")
-    scatter!(ax,1:size(M_avg,2),Ms/1000 .*M_avg[2,:], label = "M_y")
-    scatter!(ax,1:size(M_avg,2),Ms/1000 .*M_avg[3,:], label = "M_z")
-    axislegend() # position = :rt
+    scatter!(ax,time,Ms/1000 .*M_avg[1,:], label = "M_x")
+    scatter!(ax,time,Ms/1000 .*M_avg[2,:], label = "M_y")
+    scatter!(ax,time,Ms/1000 .*M_avg[3,:], label = "M_z")
+    axislegend()
 
+    # save("M_time_permalloy.png",fig)
     wait(display(fig))
 end
 
